e4ee32426d8597d0a89d667a5905e0f6e33d7ab5
hudsonst <nadinegraham@gmail.com>
2019-10-26 20:16:23 -0400

    Added tests
    Added smoke and snapshot tests

--- src/trelloyes.test.js
+++ src/trelloyes.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import List from './List';
 import Card from './Card';
 import App from './App';
 import STORE from './STORE';
+
 
 describe('Trelloyes component', () => {
     it('renders without crashing', () => {
@@ -24,4 +26,25 @@ describe('Trelloyes component', () => {
         ReactDOM.unmountComponentAtNode(div);
     });
 
+    it('renders the UI as expected', () => {
+        const tree = renderer
+          .create(<App store={STORE} />)
+          .toJSON();
+        expect(tree).toMatchSnapshot();  
+        });
+    
+    it('renders List as expected', () => {
+            const tree = renderer
+              .create(<List key='1' header='Test header' cards={['b', 'c', 'd', 'f', 'h', 'i', 'k']} />)
+              .toJSON();
+            expect(tree).toMatchSnapshot();  
+            });
+
+    it('renders Card as expected', () => {
+            const tree = renderer
+              .create(<Card title='Test Title' content='Test Content' />)
+              .toJSON();
+            expect(tree).toMatchSnapshot();  
+            });
+    
 });
